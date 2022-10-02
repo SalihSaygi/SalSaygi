288ac99440bd55eeb643a4863fd37778667b5b2f
Salih Saygi <salih.saygie@gmail.com>
2022-10-02 01:18:08 -0700

    add firebase for hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbGxNsKnEosOarY94ipbqF2RBmaeKqtzY',
+  authDomain: 'portfolio-sal.firebaseapp.com',
+  projectId: 'portfolio-sal',
+  storageBucket: 'portfolio-sal.appspot.com',
+  messagingSenderId: '908404558834',
+  appId: '1:908404558834:web:5b253e36e4056f4d6ff96b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
